57fad93b0555e6a90921ff7743c4dca789e80b01
binokular <sakurakun002@gmail.com>
2022-04-04 15:50:03 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "index.html"
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      screen: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
